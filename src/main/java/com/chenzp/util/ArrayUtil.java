44bae0702b09e2169d85a0ef947f9dfb50ac5fe9
chenzpcloud <909931227@qq.com>
2020-07-06 18:41:12 +0800

    add arrayUtil

--- src/main/java/com/chenzp/util/ArrayUtil.java
+++ src/main/java/com/chenzp/util/ArrayUtil.java
@@ -1,4 +1,15 @@
 package com.chenzp.util;
 
+
 public class ArrayUtil {
+
+    /**
+     * 指定类是否为Enum类
+     *
+     * @param clazz 类
+     * @return 是否为Enum类
+     */
+    public static boolean isEnum(Class<?> clazz) {
+        return clazz.isEnum();
+    }
 }
